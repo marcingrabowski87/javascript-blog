47c9217f7d82c970158aae0029bdd42add1ef083
Marcin Grabowski <mgrabowski87@gmail.com>
2019-09-02 10:57:55 +0200

    Remove all li from post in index.html and Add links in left sidebar by used js

--- js/script.js
+++ js/script.js
@@ -21,17 +21,20 @@ function titleClickHandler(event) {
         activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-    let clickAttribute = this.getAttribute("href");
-    clickAttribute = clickAttribute.slice(1);
+    const clickAttribute = this.getAttribute("href").slice(1);
+
 
     /* find the correct article using the selector (value of 'href' attribute) */
     const allArrticles = document.querySelectorAll('.post');
+
     let chooseArticle;
     for (let article of allArrticles) {
+
         let actualAtrributeInArticle = article.getAttribute("id");
 
-        if (clickAttribute == actualAtrributeInArticle) {
+        if (clickAttribute === actualAtrributeInArticle) {
             chooseArticle = article;
+
 
         }
     }
@@ -40,8 +43,42 @@ function titleClickHandler(event) {
 }
 
 
-const links = document.querySelectorAll('.titles a');
 
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+/* ----------------------------------------------------------------------------------------------- */
+
+
+
+
+function clearMessages() {
+    const optTitleListSelector = document.querySelectorAll('.titles');
+    for (let titleList of optTitleListSelector) {
+        titleList.innerHTML = "";
+    }
 }
+
+function addMessages(linkHtml) {
+    const titlesPostBox = document.querySelector('.titles');
+    titlesPostBox.insertAdjacentHTML('beforeend', linkHtml);
+}
+
+function getSingleArticleIDAndTitlePostAndCreateLinkHtml() {
+    const articles = document.querySelectorAll('.post');
+    for (let article of articles) {
+        const articleId = article.getAttribute('id');
+        const titlePost = article.querySelector(".post-title").innerHTML;
+        const linkHtml = '<li><a href = "#' + articleId + '"><span>' + titlePost + '</span></a></li>';
+        addMessages(linkHtml);
+    }
+}
+
+function generateTitleLinks() {
+    clearMessages();
+    getSingleArticleIDAndTitlePostAndCreateLinkHtml();
+    const links = document.querySelectorAll('.titles a');
+
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
+}
+
+generateTitleLinks();
